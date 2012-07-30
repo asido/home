b04885567354a0889241fa8c05d4ee4875a24e46
Arvydas Sidorenko <asido4@gmail.com>
2012-07-30 12:43:55 +0200

    AwesomeWM: extended CPU widget for 4 cores

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -142,7 +142,7 @@ vicious.register(memwidget, vicious.widgets.mem, "[ <span color='#0B61A4'>MEM</s
 
 --{{{ cpu
 cpuwidget = wibox.widget.textbox()
-vicious.register(cpuwidget, vicious.widgets.cpu, "[ <span color='#0B61A4'>CORES</span> $2% | $3% ]")
+vicious.register(cpuwidget, vicious.widgets.cpu, "[ <span color='#0B61A4'>CORES</span> $2% | $3% | $4% | $5% ]")
 --}}}
 --{{{ hdd
 hddwidget = wibox.widget.textbox()
